4ae23fe400933c6f86a062a540c2b83813b170d6
MaximovMaxim <maximovmaxim100500@gmail.com>
2024-01-13 03:14:48 +0300

    домашка готова

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<>(List.of(1, 1, 1, 2, 8, 20, 3, 4, 4, 5, 5, 6, 10, 7));
+        List<Integer> nums = new ArrayList<>(List.of(1, 1,3, 1, 2, 8, 20, 3, 4, 4, 5, 5, 6, 10, 7));
         List<String> strings = new ArrayList<>(List.of("один", "четыре", "два", "четыре", "два", "три", "три", "три", "четыре", "четыре"));
         Service.printOddNumbers(nums);
         System.out.println();
